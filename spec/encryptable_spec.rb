9fbe1c0cc634b6b0b61b7f5b974988bcc0665a08
Tamara Dowis <tamara.dowis@gmail.com>
2022-01-14 11:26:41 -0600

    Test: Encryptable module shift test

--- spec/encryptable_spec.rb
+++ spec/encryptable_spec.rb
@@ -3,8 +3,8 @@ require './lib/encryptable'
 
 RSpec.describe 'Encryptable' do
 
-  it 'can shift the a position' do
-    expect(Encryptable.shift_a_position('i')).to eq(y)
+  it 'can shift a position by argument spaces' do
+    expect(Encryptable.shift_a_position('i', 16).to eq('y')
   end
 
 end
