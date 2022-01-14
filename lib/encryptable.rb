6ae5752607471fae73d8a6eda456c11259cb6408
Tamara Dowis <tamara.dowis@gmail.com>
2022-01-14 15:06:59 -0600

    Feat: Encryptable shift_position method

--- lib/encryptable.rb
+++ lib/encryptable.rb
@@ -1,7 +1,16 @@
 require "pry"
 
 module Encryptable
+
+  def self.shift_position(current_position, shift)
+    total = current_position + shift
+    caculation = (total % 27)
+    return caculation unless caculation == 0
+    return current_position if caculation == 0
+  end
   # def shift_a_position
   # end
 
 end
+
+# array.inject(0, :+)
--- spec/encryptable_spec.rb
+++ spec/encryptable_spec.rb
@@ -3,18 +3,29 @@ require './lib/encryptable'
 
 RSpec.describe 'Encryptable' do
 
-  it 'can read array positions' do
-
-    expect(Encryptable.read_position(13).to eq('m')
-  end
-
-  it 'can calculate the limited positions to shift' do
-
-    expect(Encryptable.shift_position('s', 66).to eq('d')
-  end
+  # it 'can read array positions' do
+  #
+  #   expect(Encryptable.read_position(13).to eq('m')
+  # end
+  #
+  # it 'can calculate the limited positions to shift' do
+  #
+  #   expect(Encryptable.shift_position(19, 66).to eq('d')
+  # end
 
   it 'can shift a position by argument spaces' do
-    expect(Encryptable.shift_a_position('i', 16).to eq('y')
+    expect(Encryptable.shift_position(8,3)).to eq(11)
+    expect(Encryptable.shift_position(5,27)).to eq(5)
+    expect(Encryptable.shift_position(12,73)).to eq(4)
+    expect(Encryptable.shift_position(12,20)).to eq(5)
+    expect(Encryptable.shift_position(15,3)).to eq(18)
+    expect(Encryptable.shift_position(27,27)).to eq(27)
+    expect(Encryptable.shift_position(23,73)).to eq(15)
+    expect(Encryptable.shift_position(15,20)).to eq(8)
+    expect(Encryptable.shift_position(18,3)).to eq(21)
+    expect(Encryptable.shift_position(12,27)).to eq(12)
+    expect(Encryptable.shift_position(4,73)).to eq(23)
+
   end
 
 end
