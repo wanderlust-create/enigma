97a38f1d24327121c059e1922582e4da02b3b464
Tamara Dowis <tamara.dowis@gmail.com>
2022-01-14 12:13:56 -0600

    Test: Encryptable module

--- spec/encryptable_spec.rb
+++ spec/encryptable_spec.rb
@@ -3,6 +3,16 @@ require './lib/encryptable'
 
 RSpec.describe 'Encryptable' do
 
+  it 'can read array positions' do
+
+    expect(Encryptable.read_position(13).to eq('m')
+  end
+
+  it 'can calculate the limited positions to shift' do
+
+    expect(Encryptable.shift_position('s', 66).to eq('d')
+  end
+
   it 'can shift a position by argument spaces' do
     expect(Encryptable.shift_a_position('i', 16).to eq('y')
   end
